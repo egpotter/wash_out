e48df7c7c47657fea19e2330418c1a10b736027b
Eduardo Potter <epotter@ionatec.com.br>
2013-11-16 18:22:47 -0200

    bla

--- lib/wash_out/param.rb
+++ lib/wash_out/param.rb
@@ -39,6 +39,7 @@ module WashOut
     # Converts a generic externally derived Ruby value, such as String or
     # Hash, to a native Ruby object according to the definition of this type.
     def load(data, key)
+      debugger
       if !data.has_key? key
         raise WashOut::Dispatcher::SOAPError, "Required SOAP parameter '#{key}' is missing"
       end
